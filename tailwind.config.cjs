26b2ab6e2dfa621ad162966412da68c0faa0315e
Sebastien Stordeur <stordeursebastien0@gmail.com>
2022-11-10 16:31:54 +0100

    Router is prepared, pages components, basic UI components

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: "#635FC7",
+        lightPurple: "#A8A4FF",
+        black: "#000112",
+        darkGrey: "#2B2C37",
+        lines: "#3E3F4E",
+        mediumGrey: "#828FA3",
+        lightGret: "#F4F7FD",
+        white: "#FFFFFF",
+        red: "#EA5555",
+      },
+    },
   },
   plugins: [],
 };
