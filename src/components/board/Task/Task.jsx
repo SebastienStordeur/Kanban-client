24b37ab1c3d061888517794f38e4ef02707d2399
Sebastien Stordeur <stordeursebastien0@gmail.com>
2022-11-18 23:12:57 +0100

    Tasks

--- src/components/board/Task/Task.jsx
+++ src/components/board/Task/Task.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 
 const Task = (props) => {
-  console.log("Task props", props.task.subtasks);
   const completedTasks = props.task.subtasks.filter((subtask) => subtask.isCompleted === true);
-  //);
   return (
     <article className="text-left w-72 h-24 bg-white font-bold rounded-lg px-4 py-6 my-6 cursor-pointer">
       <h3 className="text-black">{props.task.title}</h3>
